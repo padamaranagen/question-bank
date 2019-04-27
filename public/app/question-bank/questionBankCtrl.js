395ce2c603fa4c4ccd0f8117f274fc0c25219efe
Nagendra Prasad <nagendramca2011@gmail.com>
2019-04-27 08:01:43 +0530

    service redirect to local to online

--- public/app/question-bank/questionBankCtrl.js
+++ public/app/question-bank/questionBankCtrl.js
@@ -2,9 +2,15 @@
 var app = angular.module('QuestionBank', []);
 
 app.controller('QuestionBankCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get('json/qb.json')
-    .success(function (response) {
-        $scope.data = response.questionbank;
-    });
+    $http({    
+        url:'https://api.jsonbin.io/b/5cc3acfde04b2c0bd81d5307',
+        method: 'GET',
+    })
+    .then(function mySuccess(response) {
+        $scope.data= response.data.questionbank;
+    },function myError(){
+        $scope.data=response.data;
+    }
+    );
 }]);
 })();//end function
